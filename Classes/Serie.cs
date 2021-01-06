523e958c955587fe6037742a0a7dfdb6b9c8a737
AriTedeschi <arijun2010@hotmail.com>
2021-01-06 15:23:30 -0300

    Parte 2 do projeto

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace DIO.Series
 {
     public class Serie : EntidadeBase
@@ -10,9 +12,39 @@ namespace DIO.Series
             this.Descricao = descricao;
             this.Ano       = Ano;
         }
+
         private Genero  Genero { get; set;}
         private string  Titulo { get; set;}
         private string  Descricao { get; set;}
         private int     Ano { get; set;}
+
+        public override string ToString()
+        {
+            string re = "";
+            re += "Gênero        : " + this.Genero + Environment.NewLine;
+            re += "Titulo        : " + this.Titulo + Environment.NewLine;
+            re += "Descrição     : " + this.Descricao + Environment.NewLine;
+            re += "Ano de Início : " + this.Ano + Environment.NewLine;
+            return re;
+        }
+
+        #region Encapsulamento
+        public int    getId()
+        {
+            return this.Id;
+        }
+        public string getTitulo()
+        {
+            return this.Titulo;
+        }
+        public string getDescricao()
+        {
+            return this.Descricao;
+        }
+        public int    getAno()
+        {
+            return this.Ano;
+        }
+        #endregion
     }
 }
